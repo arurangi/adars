cbc9127db2524994f4b4acebb62625e048b2f0b8
Arsene Rurangiza <arurangi@shi-r9-p2.s19.be>
2023-10-02 16:35:06 +0200

    docu: improved

--- playground/client.cpp
+++ playground/client.cpp
@@ -16,32 +16,39 @@ int main()
     struct sockaddr_in serv_addr;
     char hello[] = "Hello from client";
     char buffer[1024] = {0};
+
+    // 1. create socket
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\n Socket creation error \n");
         return -1;
     }
     
+    // 2. connect to remote server
+    // -- initialize address structure
     memset(&serv_addr, 0, sizeof(serv_addr));
-    
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(SERVER_PORT);
-    
-    // Convert IPv4 and IPv6 addresses from text to binary form
+    // -- -- convert IPv4 and IPv6 addresses from text to binary form
     if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
     {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
-    
+    // -- connect
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
         printf("\nConnection Failed \n");
         return -1;
     }
+
+    // 3. send message
     send(sock , hello , strlen(hello) , 0 );
     printf("Hello message sent\n");
+
+    // 4. read response
     valread = read( sock , buffer, 1024);
     printf("%s\n",buffer );
+
     return 0;
 }
